19c3be6ae7e23f6a3689d0eabd6b6fe20dfe9da6
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-20 16:50:06 -0700

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,51 +3,31 @@
 import sys
 
 
-def print_logs(file_size: int, status_codes: dict):
-    """Print logs
-        Args: file_size (int): Total file size
-                status_codes (dict): Dictionary of status codes
-                Returns: None
-        """
+def print_metrics(file_size: int, status_codes: dict):
+    """ print the logs after parsing """
     print("File size: {}".format(file_size))
-    for key, value in sorted(status_codes.items()):
-        if (value > 0):
-            print("{}: {}".format(key, value))
+    for k, v in status_codes.items():
+        if (v > 0):
+            print("{}: {}".format(k, v))
 
 
-def parse_log():
-    """Parse logs about status codes and file size from stdin
-        Args: None
-                Returns: None
-        """
-    file_size = 0
-    status_codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0}
-    current_line = 0
+file_size = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+count = 0
 
-    try:
-        for line in sys.stdin:
-            data = line.split()
-            if (len(data) < 2):
-                continue
-            file_size += int(data[-1])
-            status = data[-2]
-            if (status in status_codes):
-                status_codes[status] += 1
-            current_line += 1
-            if (current_line % 10 == 0):
-                print_logs(file_size, status_codes)
-    except KeyboardInterrupt:
-        pass
-    print_logs(file_size, status_codes)
-
-
-if __name__ == "__main__":
-    parse_log()
+try:
+    for line in sys.stdin:
+        log = line.split()
+        if (len(log) < 2):
+            continue
+        file_size += int(log[-1])
+        code = log[-2]
+        if code in status_codes:
+            status_codes[code] += 1
+        count += 1
+        if count % 10 == 0:
+            print_metrics(file_size, status_codes)
+except KeyboardInterrupt:
+    pass
+print_metrics(file_size, status_codes)
