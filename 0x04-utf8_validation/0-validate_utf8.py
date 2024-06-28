8271bfa590078534a2eb718a608a0c34b66ba1bf
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-27 17:49:47 -0700

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,7 @@
 
 def validUTF8(data):
     """  determines if a given data set represents a valid UTF-8 encoding """
-    if type(data) is not list or any(not isinstance(element, int) for element in data):
+    if type(data) is not list or any(not isinstance(i, int) for i in data):
         return False
     data_bytes = [byte & 0xFF for byte in data]
     try:
