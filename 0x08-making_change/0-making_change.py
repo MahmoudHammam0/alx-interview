9ceb0e030060adfcbf3b5a3a8c00143e726d5f88
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-26 02:32:09 +0000

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,15 +3,17 @@
 
 
 def makeChange(coins, total):
+    """ fewest number of coins needed to meet a given amount """
+    if not coins:
+        return -1
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        for amount in range(coin, total + 1):
-            if dp[amount - coin] != float('inf'):
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if total == 0:
+            return change
+    return -1
