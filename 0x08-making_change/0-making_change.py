074c6262bab78ba7144e4d06f6c5aaae819e4e55
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-26 02:34:07 +0000

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,12 +8,13 @@ def makeChange(coins, total):
         return -1
     if total <= 0:
         return 0
+    coins = sorted(coins, reverse=True)
     change = 0
-    coins = sorted(coins)[::-1]
+    original_total = total
     for coin in coins:
-        while coin <= total:
+        while total >= coin:
             total -= coin
             change += 1
-        if total == 0:
-            return change
+    if total == 0:
+        return change
     return -1
