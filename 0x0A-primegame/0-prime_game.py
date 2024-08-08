2d4168852f76213ab0764e8e54108f09b9f799bd
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-08-08 15:43:26 -0700

    0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
 """ Prime game module """
 
-def prime_counter(n):
-    """Count the number of prime numbers up to n """
-    if n < 2:
-        return 0
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False
-    for i in range(2, int(n**0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, n + 1, i):
-                is_prime[j] = False
-    return sum(is_prime)
 
-def is_winner(x, nums):
-    """Determine the winner of the prime game."""
-    if x <= 0 or nums is None or x != len(nums):
+def prime_counter(num):
+    """ check if is it prime """
+    prime_nums = []
+    sieve = [True] * (num + 1)
+    for n in range(2, num + 1):
+        if sieve[n]:
+            prime_nums.append(n)
+            for multiple in range(n, num + 1, n):
+                sieve[multiple] = False
+    return prime_nums
+
+
+def isWinner(x, nums):
+    """ returns the winner """
+    if x is None or nums is None or x == 0 or not nums:
         return None
 
-    maria_wins = sum(prime_counter(n) % 2 for n in nums)
-    ben_wins = x - maria_wins
+    maria_wins = 0
+    ben_wins = 0
+
+    for limit in range(x):
+        prime_numbers = prime_counter(nums[limit])
+        if len(prime_numbers) % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return 'Maria'
     elif ben_wins > maria_wins:
         return 'Ben'
-    return None
-
+    else:
+        return None
