90c0dc190bbe9f959ff692981b5543d69d5ae2a7
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-08-08 15:34:44 -0700

    0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,50 +1,29 @@
 #!/usr/bin/python3
-""" Prime Game module """
+""" Prime game module """
 
+def prime_counter(n):
+    """Count the number of prime numbers up to n using the Sieve of Eratosthenes."""
+    if n < 2:
+        return 0
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    return sum(is_prime)
 
-def isWinner(x, nums):
-    def sieve(n):
-        """ Return a list of primes up to n using the Sieve of Eratosthenes"""
-        is_prime = [True] * (n + 1)
-        is_prime[0] = is_prime[1] = False
-        for start in range(2, int(n**0.5) + 1):
-            if is_prime[start]:
-                for multiple in range(start * start, n + 1, start):
-                    is_prime[multiple] = False
-        return [num for num, prime in enumerate(is_prime) if prime]
+def is_winner(x, nums):
+    """Determine the winner of the prime game."""
+    if x <= 0 or nums is None or x != len(nums):
+        return None
 
-    def winner(n):
-        """ Determine the winner for a given n. """
-        if n < 2:
-            return "Ben"
-
-        primes = sieve(n)
-        primes_set = set(primes)
-        available = set(range(1, n + 1))
-
-        turn = 0
-        while primes_set:
-            current_prime = next((p for p in primes if p in available), None)
-            if current_prime is None:
-                return "Ben" if turn == 0 else "Maria"
-            to_remove = set(range(current_prime, n + 1, current_prime))
-            available.difference_update(to_remove)
-            primes_set.difference_update(to_remove)
-            turn = 1 - turn
-
-        return "Ben" if turn == 0 else "Maria"
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if winner(n) == "Maria":
-            maria_wins += 1
-        else:
-            ben_wins += 1
+    maria_wins = sum(prime_counter(n) % 2 for n in nums)
+    ben_wins = x - maria_wins
 
     if maria_wins > ben_wins:
-        return "Maria"
+        return 'Maria'
     elif ben_wins > maria_wins:
-        return "Ben"
+        return 'Ben'
     return None
+
